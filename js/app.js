c44bf829ebee9c28530f9647165e0b53c77893e7
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-21 10:25:29 -0300

    creamos un  contacto con la info del forumulario

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
+// importa una clase desde una direccion de archivo
 import Contacto from "./classContacto.js";
 
-// el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
-function agregarContacto(){
-   const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
-    //mostrar ventana modal
-    modalCrearContacto.show();
 
+// el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
+function abrirModalContacto() {
+    const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
+    //mostrar ventana modal, desde js, no con data-bs-target y data-bs-toggle
+    modalCrearContacto.show();
 }
+
+function crearContacto() {
+    //1- traer todos los datos del formulario validados
+
+    //2- crear objeto contacto
+    const nuevoContacto = new Contacto(inputNombre.value, inputApellido.value, inputTelefono.value, inputEmail.value, inputImagen.value, inputNotas.value)
+
+    console.log(nuevoContacto)
+    //3- almacenar el objeto en la agenda
+    agenda.push(nuevoContacto)
+    console.log(agenda)
+}
+
 //el usuario completa el form y debo crear un objeto contacto
 
 //declaro variables
 const btnAgregarContacto = document.getElementById('btnAgregarContacto');
 const formularioCrearContacto = document.querySelector('form');
 
+// valores el input xa crear contacto
+const agenda = [];
+const inputNombre = document.querySelector('#nombre')
+const inputApellido = document.querySelector('#apellido')
+const inputEmail = document.querySelector('#email')
+const inputTelefono = document.querySelector('#telefono')
+const inputImagen = document.querySelector('#imagen')
+const inputNotas = document.querySelector('#notas')
+
+
 //manejadores de eventos
-btnAgregarContacto.addEventListener('click', agregarContacto)
-formularioCrearContacto.addEventListener('submit', (e)=>{
+btnAgregarContacto.addEventListener('click', abrirModalContacto)
+
+formularioCrearContacto.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('aqui deberia crear un objeto contacto')
     //crear un objeto Contacto
-    const nuevoContacto = new Contacto('juan', 'perez','23423423','juanperez@mail.com')
-    console.log(nuevoContacto)
+    crearContacto()
 })
--- js/classContacto.js
+++ js/classContacto.js
@@ -1,16 +1,29 @@
-export default class Contacto{
+export default class Contacto {
     #id;
     #nombre;
     #apellido;
     #telefono;
     #email;
-    constructor(nombre,apellido,telefono,email){
+    #imagen;
+    #notas;
+    constructor(nombre, apellido, telefono, email, imagen, notas) {
         this.#id = crypto.randomUUID();
         this.#nombre = nombre;
         this.#apellido = apellido;
         this.#telefono = telefono;
         this.#email = email;
+        this.#imagen = imagen;
+        this.#notas = notas;
     }
-        
-        //agregar getter y setter
+
+    // Getter
+    get getNombre() {
+        return this.#nombre;
+    }
+
+    // Setter
+    set setNombre(nuevoNombre) {
+        this.#nombre = nuevoNombre;
+    }
+
 }
